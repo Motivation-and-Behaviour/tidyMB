b5961b34347d90129d2401b49ffbb63253ba01fc
Philip Parker <philipparker@MacBook-Pro.local>
2020-09-13 19:30:38 +1000

    Fixed URL browser.

--- R/utilities.R
+++ R/utilities.R
@@ -1,12 +1,11 @@
 
 #' open_notion
 #'
-#' @importFrom  rstudioapi viewer
 #'
 #' @return
 #' @export
 #'
 #' @examples
 open_notion <- function() {
-  rstudioapi::viewer("https://www.notion.so/IPPE-Collaborative-Workflow-b7c2e7a025af446ab89aa1ad8dff0cca")
+  browseURL("https://www.notion.so/IPPE-Collaborative-Workflow-b7c2e7a025af446ab89aa1ad8dff0cca")
 }
