626bb75bb4362215baeb95299f59ef7c8eb492e7
Philip Parker <philipparker@MacBook-Pro.local>
2020-09-13 19:26:10 +1000

    Add-in to open Wiki

---
+++ R/utilities.R
@@ -0,0 +1,12 @@
+
+#' open_notion
+#'
+#' @importFrom  rstudioapi viewer
+#'
+#' @return
+#' @export
+#'
+#' @examples
+open_notion <- function() {
+  rstudioapi::viewer("https://www.notion.so/IPPE-Collaborative-Workflow-b7c2e7a025af446ab89aa1ad8dff0cca")
+}
