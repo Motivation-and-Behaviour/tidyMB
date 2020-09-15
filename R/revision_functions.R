41c284b10f31de9f8068002a9c527689e1a1eecf
Taren Sanders <taren.sanders@acu.edu.au>
2020-09-15 14:04:51 +1000

    Adding mnb object for formatting plots

--- R/revision_functions.R
+++ R/revision_functions.R
@@ -2,18 +2,21 @@
 #'
 #' @param address string url or file location of knited html
 #' @param id the id from a html tag
+#' @param current the current version of the paragraph (optional)
 #'
 #' @importFrom glue glue
 #' @importFrom rvest html_nodes html_text
 #' @importFrom xml2 read_html
 #' @importFrom stringr str_trim
 #' @importFrom magrittr "%>%"
+#' @importFrom stringr str_replace
 #'
 #' @return
 #' @export
 #'
 #' @examples
-read_manuscript <- function(address,id){
+#'
+read_manuscript <- function(address,id, current){
   # Test for missing or non-character id
   if(missing(id)|!is.character(id)) stop("Please specify an id string")
   # Read in data and parse
@@ -25,7 +28,21 @@ read_manuscript <- function(address,id){
   tmp = tmp %>%
     rvest::html_text() %>%
     stringr::str_trim()
-  return(tmp)
+
+  # Compare to current
+  if(missing(current)) {
+    return(tmp)
+  } else {
+    lst <- strsplit(c(tmp = tmp,current = current), "(?<=[.])\\s", perl = TRUE)
+    un1 <-unique(unlist(lst))
+    diffs <- lapply(lst, setdiff,x=un1)
+    repltxt <- paste0(paste0("*",diffs$current),"*")
+    for(i in 1:length(diffs$current)){
+      tmp <- stringr::str_replace(tmp, diffs$current[i],repltxt[i])
+    }
+    return(tmp)
+  }
+
 }
 
 
@@ -61,6 +78,4 @@ span_remove_addin <- function() {
   text = stringr::str_replace(selection$text,"<\\s*span[^>]*>(.*?)<\\s*/\\s*span>","\\1")
   rstudioapi::insertText(text)
 }
-
-
 
