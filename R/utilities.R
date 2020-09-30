ba3c595850a0ce4373248cb31841cc044297efe0
Taren Sanders <taren.sanders@acu.edu.au>
2020-09-30 10:12:52 +1000

    Revert "Adding mnb object for formatting plots"
    This reverts commit 41c284b10f31de9f8068002a9c527689e1a1eecf.

--- R/utilities.R
+++ R/utilities.R
@@ -10,13 +10,3 @@
 open_notion <- function() {
   browseURL("https://www.notion.so/IPPE-Collaborative-Workflow-b7c2e7a025af446ab89aa1ad8dff0cca")
 }
-
-mnb <- theme_fivethirtyeight() %+replace%
-  theme(legend.position = "none",
-        text = element_text(family = "Times"),
-        axis.title.x = element_text(),
-        axis.title.y = element_text(),
-        panel.background = element_blank(),
-        plot.background = element_blank(),
-        strip.background = element_blank(),
-        legend.background = element_blank())
